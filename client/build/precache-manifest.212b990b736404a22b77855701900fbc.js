7597986172326bc915bfd3f7e9b42947c5c4eb90
Dishank Oza <oa.dishank1@gmail.com>
2020-11-01 21:19:49 +0530

    final commit

---
+++ client/build/precache-manifest.212b990b736404a22b77855701900fbc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55b3728ca33d969b5c6f47dbdc78a815",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2553779eacbcf0f66e79",
+    "url": "/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "ff358bbcd18030907207",
+    "url": "/static/js/2.4d15e837.chunk.js"
+  },
+  {
+    "revision": "50949285a495ff50d33a",
+    "url": "/static/js/3.ed3250a5.chunk.js"
+  },
+  {
+    "revision": "a208b8fb32dd3628211b",
+    "url": "/static/js/4.108727ac.chunk.js"
+  },
+  {
+    "revision": "134fdf7cbcf8d7f9a1f3",
+    "url": "/static/js/5.edb78053.chunk.js"
+  },
+  {
+    "revision": "30bcd88ea95d0b090c14",
+    "url": "/static/js/6.5abf35ce.chunk.js"
+  },
+  {
+    "revision": "d4a340d27abd5779c613",
+    "url": "/static/js/7.5aec73e4.chunk.js"
+  },
+  {
+    "revision": "dff1d90e72e2d6b3c324",
+    "url": "/static/js/8.07958639.chunk.js"
+  },
+  {
+    "revision": "fbb722dcf68db5efb63c",
+    "url": "/static/js/9.cc73ac7a.chunk.js"
+  },
+  {
+    "revision": "2553779eacbcf0f66e79",
+    "url": "/static/js/main.21562cef.chunk.js"
+  },
+  {
+    "revision": "69efe44774c47227eca8",
+    "url": "/static/js/runtime~main.c20e8d47.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
